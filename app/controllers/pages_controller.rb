76397bb6bb5b6caa0cac4c368ba9d791e4f78b98
Zuza Zuber <zuza@lewagon.org>
2020-03-25 10:21:29 +0100

    create table view and sql query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
+    query = <<-SQL
+      SELECT *
+      FROM orders
+      JOIN users ON users.id = orders.user_id
+      JOIN accounts ON accounts.id = users.account_id
+      WHERE accounts.domestic = False;
+    SQL
+
+    @international_orders = ActiveRecord::Base.connection.execute(query).to_a
   end
+
 end
