d83e58ca9373a262f71809ffdd11d28c0d255384
Zuza Zuber <zuza@lewagon.org>
2020-03-25 14:46:58 +0100

    update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'user_orders', to: 'pages#user_orders', as: :user_orders
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
